5fd4a675026e7b7ca34ba65b0f1493bedd51b33f
patrickasdf <173692702+patrickasdf@users.noreply.github.com>
2025-03-31 21:23:25 +1000

    Book constructor stores unique id and returns via idInfo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call contstructor");
@@ -6,11 +8,20 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = crypto.randomUUID();
     this.info = function() {
         return(this.title + " by " + this.author + ", " + this.pages +
-        ", " + this.read)
+        ", " + this.read + " id " + this.id)
     };
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "295 pages", "not read yet");
+const theBobbit = new Book("The Bobbit", "J.R.R. Tolkein", "295 pages", "not read yet");
 console.log(theHobbit.info());
+console.log(theBobbit.info());
+console.log(theBobbit.info());
+
+function addBookToLibrary() {
+    const newBook = new Book(title, author, pages, read);
+
+}
